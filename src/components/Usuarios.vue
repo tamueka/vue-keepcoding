5c75e58770330f9506de4721d2a7ad4f4a4727e4
tamueka <tamueka@hotmail.com>
2020-04-22 23:54:36 +0200

    modificamos app.vue + eliminamos el componente por defecto HelloWorld.vue + creamos componente Usuarios.vue (dentro  un array y lo pintamos ) + cambiamos enrutado en index.js

---
+++ src/components/Usuarios.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+  <div>
+    <h1 class="red">Usuarios:</h1>
+    <ul v-for="user in users" :key="user.id">
+      <li>{{user}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Usuarios',
+  data() {
+    return {
+      users: ['Luis', 'Juan', 'Pepe'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .red {
+    color: red
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Usuarios from '@/components/Usuarios';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      path: '/usuarios',
+      name: 'Usuarios',
+      component: Usuarios,
     },
   ],
 });
