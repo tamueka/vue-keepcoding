bdde76638ffccbe1515bd3dee1111d625e600e13
tamueka <tamueka@hotmail.com>
2020-04-23 01:07:08 +0200

    instalamos axios para realizar peticiones http + importamos axios en el componente Usuarios + creamos componente Usuario, lo lalamamos en Usuarios y le damos estilos

---
+++ src/components/Usuario.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="user">
+    <img :src="user.avatar" alt="">
+    <div>{{user.first_name}} {{user.last_name}}</div>
+    <div>{{ user.email }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'usuario',
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -2,20 +2,42 @@
 
 <template>
   <div>
-    <h1 class="red">Usuarios:</h1>
+    <h1 class="red">Usuarios {{ total }}:</h1>
+    <button @click="loadUsers">Cargar Usuarios</button>
     <ul v-for="user in users" :key="user.id">
-      <li>{{user}}</li>
+      <usuario :user='user'></usuario>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Usuario from './Usuario';
+
 export default {
   name: 'Usuarios',
+  components: {
+    Usuario,
+  },
   data() {
     return {
-      users: ['Luis', 'Juan', 'Pepe'],
+      users: [],
+      page: 1,
+      total: 0,
+      totalPages: 0,
     };
+  },
+  methods: {
+    loadUsers() {
+      axios.get('https://reqres.in/api/users')
+        .then((response) => {
+          const { data, page, total, totalPages } = response.data;
+          this.users = data;
+          this.page = page;
+          this.total = total;
+          this.totalPages = totalPages;
+        });
+    },
   },
 };
 </script>
